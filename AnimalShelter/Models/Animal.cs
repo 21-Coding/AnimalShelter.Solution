2bbda88ead9231b0d6d4f23214714f17675b88c4
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-12 15:21:24 -0700

    ➕ add models.

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace   AnimalShelter.Models
+{
+    public class Animal
+    {
+        
+    }
+}
