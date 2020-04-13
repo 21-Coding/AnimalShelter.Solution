18da10cc408a970ffb7a083139ee997df0438d95
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-12 18:43:57 -0700

    ➕ update int to string under 'Type'.

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -10,7 +10,7 @@ namespace   AnimalShelter.Models
         public int AnimalId {get; set;}
         public string Name {get; set;}
         public int Age {get;  set;}
-        public int Type {get; set;}
+        public string Type {get; set;}
         
     }
 }
