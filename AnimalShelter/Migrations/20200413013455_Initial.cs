3891594283cde2f62d73a5f061aa0b3adaf4ee66
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-12 18:48:51 -0700

    ➕ add Migrations.

---
+++ AnimalShelter/Migrations/20200413013455_Initial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    AnimalId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Type = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.AnimalId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, 4, "Leonitus", "Kangaroo" },
+                    { 2, 21, "Rudy", "Monitor Lizard" },
+                    { 3, 6, "Ethan", "Billy Goat" },
+                    { 4, 1, "Samantha", "Manatee" },
+                    { 5, 8, "Stevent", "Starfish" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Animals");
+        }
+    }
+}
