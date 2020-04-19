8c7d915cb63917305970356fda88792337662eae
DominiqueYoumans <ddyoumans@icloud.com>
2020-04-18 21:14:02 -0700

    update models.

---
+++ AnimalShelter/Migrations/20200419041153_SeedData.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                column: "Type",
+                value: "Cat");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                column: "Type",
+                value: "Dog");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                column: "Type",
+                value: "Cat");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                column: "Type",
+                value: "Dog");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                column: "Type",
+                value: "Cat");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1,
+                column: "Type",
+                value: "Kangaroo");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2,
+                column: "Type",
+                value: "Monitor Lizard");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3,
+                column: "Type",
+                value: "Billy Goat");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4,
+                column: "Type",
+                value: "Manatee");
+
+            migrationBuilder.UpdateData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5,
+                column: "Type",
+                value: "Starfish");
+        }
+    }
+}
